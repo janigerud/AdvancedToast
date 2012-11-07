923068b6cdda700b399a13add808750f0047c60a
jan@macMini <jan@macMini>
2012-11-07 23:39:59 +0100

    init commit, project created

---
+++ src/com/example/AdvancedToast.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jan
+ * Date: 11/7/12
+ * Time: 10:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdvancedToast {
+}
