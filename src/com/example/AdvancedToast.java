1ee06ed702c5fa6be60f054a65707a1e9a5f80e0
jan@macMini <jan@macMini>
2012-11-18 21:23:24 +0100

    Used setview on a normal Toast

--- src/com/example/AdvancedToast.java
+++ src/com/example/AdvancedToast.java
@@ -13,35 +13,44 @@ import android.widget.Toast;
  * User: jan
  * Date: 11/7/12
  * Time: 10:48 PM
- * To change this template use File | Settings | File Templates.
+ * Create a simple Toast with adjustable textsise
  */
 public class AdvancedToast {
 
     private Context context = null;
     private LinearLayout ll = null;
     private Toast toast = null;
-    AdvancedToast(Context context){
-        this.context = context;
-        ll = new LinearLayout(context);
-        ll.setVisibility(View.VISIBLE);
-        LinearLayout.LayoutParams lpr = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-        ll.setLayoutParams(lpr);
+    private AdvancedToast() {};
 
+    //crete view programmicly, more portable
+    static private View makeToastView(Context context,String text, float textSize){
+    	LinearLayout ll = new LinearLayout(context);
+        ll.setVisibility(View.VISIBLE);
+        LinearLayout.LayoutParams lpr = new LinearLayout.LayoutParams(
+        		LinearLayout.LayoutParams.WRAP_CONTENT,
+        		LinearLayout.LayoutParams.WRAP_CONTENT);
+        ll.setLayoutParams(lpr);
+        
         TextView tv = new TextView(context);
-        tv.setText("I have made a advance Toast!!");
+        tv.setText(text);
         tv.setTextColor(Color.RED);
-        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,20.0f);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
         tv.setVisibility(View.VISIBLE);
         tv.setId(1);
         ll.addView(tv);
+    	return (View)ll;
+    }
+    /* 
+    	Create and initiate a Tost with more adjustable textsise
+	@param showDuration MUST be  Toast.LENGTH_LONG or Toast.LENGTH_SHORT
+    */	
+    public static Toast makeToast(Context context, String text, float textSize, int showDuration){
+    	Toast toast = new Toast(context);
+    	toast.setView(makeToastView(context, text,textSize));
+    	toast.setDuration(showDuration);
+    	return toast;
+    }
 
-    }
-    public void makeToast(){
-        toast = new Toast(context);
-        toast.setView(ll.getRootView());
-        toast.setDuration(Toast.LENGTH_LONG);
-        toast.show();
-    }
 
 
 }
--- src/com/example/MyActivity.java
+++ src/com/example/MyActivity.java
@@ -2,6 +2,8 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+//import com.example.AdvancedToast; 
 
 public class MyActivity extends Activity {
     /**
@@ -16,7 +18,8 @@ public class MyActivity extends Activity {
     @Override
     public void onResume(){
         super.onResume();
-        AdvancedToast at = new AdvancedToast(this);
-        at.makeToast();
+        Toast t = AdvancedToast.makeToast(this, "Broken movie file, please download it againg", 40.0f, Toast.LENGTH_LONG);
+        t.show();
+
     }
 }
